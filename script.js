12c0be39b12dfecf9c00815d5977eb114457a577
AljassimAkram <akramwljasem1995@gmail.com>
2026-05-22 01:05:04 +0200

    add html, Add hover effects and language toggle

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+loadHTML("about-me.html", "about");
+loadHTML("skills.html", "skills");
+loadHTML("projects.html", "projects");
+loadHTML("oppinion-about-me.html", "oppinion");
+loadHTML("contact-me.html", "contact");
+
+function loadHTML(file, elementId) {
+  fetch(file)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(elementId).innerHTML = data;
+    });
+}
+
+function toggleLanguage(lang) {
+  let en = document.getElementById("en");
+  let de = document.getElementById("de");
+
+  en.classList.remove("active");
+  de.classList.remove("active");
+
+  document.getElementById(lang).classList.add("active");
+}
